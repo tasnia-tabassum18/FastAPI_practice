8b89557a5392ff5a29c9b01d57c942b51b6a1236
Tasnia Tabassum Oishee <104383410+tasnia-tabassum18@users.noreply.github.com>
2023-11-21 11:15:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 app = FastAPI() #instantiate app
 
 #setup route
@@ -11,9 +12,31 @@ async def post():
 @app.put("/")
 async def put():
     return{"message":"hey from put route uWu"}
-@app.get("/items")
-async def list_items():
-    return{"message":"list items"}
-@app.get("/items/{item_id}")
-async def get_item(item_id:int):
-    return{"item_id":item_id}
+# @app.get("/items")
+# async def list_items():
+#     return{"message":"list items"}
+# @app.get("/items/{item_id}")
+# async def get_item(item_id:int):
+#     return{"item_id":item_id}
+@app.get("/users")
+async def list_users():
+    return{"message":"list users"}
+
+@app.get("/users/me")
+async def get_current_user():
+    return{"message":"This is the current user"}
+@app.get("/users/{user_id}")
+async def get_item(user_id:str):
+    return{"user_id":user_id}
+
+class FoodEnum(str, Enum):
+    fruits= "fruits"
+    vegetables="vegetables"
+    dairy = "dairy"
+@app.get("/foods/{food_name}")
+async def get_food_name(food_name:FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return{"food_name":food_name, "message":"you're healthy"}
+    if food_name == FoodEnum.fruits:
+        return{"food_name":food_name, "message":"good to eat fruits"}
+    return{"food_name":food_name, "message":"I like dairy"}
